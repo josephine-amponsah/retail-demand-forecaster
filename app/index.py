3c09e9548a423a43f62603aca3d6f6d07a783eaf
Josephine Amponsah Baah <97001821+ladyjossy77@users.noreply.github.com>
2022-10-21 11:30:24 +0000

    sales analysis eda

---
+++ app/index.py
@@ -0,0 +1,40 @@
+import dash 
+from dash import Dash, html, dcc
+import plotly.express as px
+import pandas as pd
+import dash_bootstrap_components as dbc
+from dash.dependencies import Input, Output
+
+
+# from app.pages import sales
+
+app = dash.Dash(__name__, use_pages=True, external_stylesheets=[
+                dbc.themes.BOOTSTRAP, dbc.icons.BOOTSTRAP])
+# app = dash.Dash(external_stylesheets=[dbc.themes.BOOTSTRAP, dbc.icons.BOOTSTRAP])
+
+app.layout = html.Div([
+    dbc.Row([
+            dbc.Col(
+                html.Div([
+                    dbc.Nav("LOGO"),
+                    html.Br(),
+                    html.Div('Dashboard'),
+                    html.Div(
+                        [
+                            dbc.Row(
+                                [dbc.NavLink(page["name"], href=page['path'], className="nav-subtext")],)
+                            for page in dash.page_registry.values()
+                            if page["path"].startswith("/dashboard")
+                        ]
+                    ),
+                ], className='nav-card '), style={"height": "100%"},
+                width=2
+            ),
+            dbc.Col(dash.page_container)
+            ])
+]
+)
+
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
---
+++ app/pages/projections.py
@@ -0,0 +1,52 @@
+import dash
+from dash import Dash, html, dcc
+import plotly.express as px
+import pandas as pd
+import dash_bootstrap_components as dbc
+
+# app = Dash(__name__)
+dash.register_page(__name__, path="/dashboard/sales-stats")
+#app = dash.Dash(external_stylesheets=[dbc.themes.BOOTSTRAP, dbc.icons.BOOTSTRAP])
+
+revSummary = ['Revenue', 'Transactions', 'Avg. Purchase',  'Profit']
+
+layout = html.Div([
+    dbc.Row([
+        dbc.Row([
+            dbc.Col([
+                  html.Div([
+                      html.I(className="bi bi-search"),
+                      html.I(className="bi bi-bell-fill"),
+                      html.I(className="bi bi-person-circle"),
+                  ], className='icon-bar'),
+                  ],  width=3)
+        ], justify="end"),
+        html.Br(),
+        dbc.Row(style={'height': '20px'}),
+        html.Br(),
+        dbc.Row([
+            dbc.Col(dbc.Card(
+                children=[html.Div(title), html.Div("value", className='value'), html.Div("description")], className='summary-cards',),
+                width=3,)
+            for title in revSummary
+        ]),
+        html.Br(),
+        dbc.Row(style={'height': '20px'}),
+        html.Br(),
+        dbc.Row([
+            dbc.Col([
+                dbc.Row([
+                    dcc.Graph(id="bar-trans-count")
+                ]),
+                dbc.Row([
+                    dcc.Graph(id="bar-sales-rev")
+                ])
+            ], width=9),
+            dbc.Col([
+                dbc.Card(children=[html.Div("Charts")])
+            ], width=3)
+        ], style={"height": "100vh"})
+    ], className="dashboard")
+
+
+])
