fb18bd2b2b7c8540844c466f118e343b71b2c52e
Josephine Amponsah Baah <97001821+ladyjossy77@users.noreply.github.com>
2022-12-01 09:27:50 +0000

    vbn

--- app/app.py
+++ app/app.py
@@ -38,5 +38,5 @@ app.layout = html.Div([
 
 if __name__ == '__main__':
     #app.run_server(debug=True, host='127.0.0.1', port=8080)
-    serve(app, host='127.0.0.1', port=8080)
+    serve(app, host='0.0.0.0', port=8080)
 
