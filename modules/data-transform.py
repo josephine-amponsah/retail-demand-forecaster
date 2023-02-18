e56933172d955777c23ca7bb9353e0a728b7d678
Josephine Amponsah Baah <97001821+ladyjossy77@users.noreply.github.com>
2023-02-18 20:56:33 +0000

    data restructuring

--- modules/data-transform.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import numpy as np
-import scipy
-import statsmodels as st
-import matplotlib.pyplot as plt
-import seaborn as sns
-import sklearn as sk
-import keras
-
--- modules/visuals.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import numpy as np
-import plotly.express as pe
-import seaborn as sns
-
-
-def summaryCard(df, value, agg, group):
-    data = data.groupby(group)[value].agg()
-    
