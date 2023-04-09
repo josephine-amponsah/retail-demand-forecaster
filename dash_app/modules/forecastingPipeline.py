bfcd9cf4a00a679e520ce09f2cd01f26c535d27c
Josephine Amponsah Baah <josephine.amponsah98@gmail.com>
2023-04-09 04:06:53 +0000

    define prediction function in forecasting module

--- dash_app/modules/forecastingPipeline.py
+++ dash_app/modules/forecastingPipeline.py
@@ -1,26 +1,24 @@
-import sktime
-from sktime.forecasting.compose._multiplexer import MultiplexForecaster
-from sktime.forecasting.theta import ThetaForecaster
-# from sktime.forecasting.exp_smoothing import ExponentialSmoothing
-from sktime.forecasting.ets import AutoETS
-from sktime.forecasting.naive import NaiveForecaster
-# from sktime.forecasting.trend import PolynomialTrendForecaster
-from sktime.forecasting.compose import ForecastingPipeline
+# import sktime
+# from sktime.forecasting.compose._multiplexer import MultiplexForecaster
+# from sktime.forecasting.theta import ThetaForecaster
+# # from sktime.forecasting.exp_smoothing import ExponentialSmoothing
+# from sktime.forecasting.ets import AutoETS
+# from sktime.forecasting.naive import NaiveForecaster
+# # from sktime.forecasting.trend import PolynomialTrendForecaster
+# from sktime.forecasting.compose import ForecastingPipeline
 import pandas as pd
+import sklearn 
+import joblib
 
-pipe = ForecastingPipeline(steps =[
-    MultiplexForecaster(forecasters=[
-    ("ets", AutoETS()),
-    ("theta", ThetaForecaster()),
-    ("naive", NaiveForecaster())] )
-])
-def transform(df, columns, values):
-    df = df.groupby(columns)[values].sum()
-    return df
+model = joblib.load('forecast.pkl')
 
-def forecast(fh, df, start):
-    df = transform(df, columns, values)
-    pred = model.predict()
-    final = pred[[start]]
+# def transform(df, columns, values):
+#     df = df.groupby(columns)[values].sum()
+
+#     return df
+
+def forecast(fh):
+    pred = model.predict(fh)
+    final = pred.reset_index()
     return final
 
