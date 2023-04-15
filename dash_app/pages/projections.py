97a0a3077e81459d2455fbf3bb48d0dba82470f1
Josephine Amponsah Baah <josephine.amponsah98@gmail.com>
2023-04-15 09:01:46 +0000

    resolve model ouput error, completed callbacks for projections, updated UI

--- dash_app/pages/projections.py
+++ dash_app/pages/projections.py
@@ -6,9 +6,11 @@ import dash_bootstrap_components as dbc
 from dash import dash_table
 import json
 from sktime.forecasting.base import ForecastingHorizon
-from modules import forecastingPipeline as forecaster
+from modules import forecastingPipeline
 from datetime import datetime
 from dash.exceptions import PreventUpdate
+import numerize
+from numerize import numerize
 
 dash.register_page(__name__, path = '/projections')
 
@@ -87,10 +89,10 @@ layout = html.Div([
             html.Div(
                 [
                     html.Div([
-                        html.H6("Warehouses", className="card-title"),
-                        
-                    ], className="card-body")
-                ], className= "card bg-light mb-3 projections-tables", id="whse-projection"
+                        html.H6("Top 10 Categories", className="card-title"),
+                    ], className="card-body"),
+                    html.Div([],  id="cat-projection", className = 'table-box')
+                ], className= "card bg-light mb-3 projections-tables details-table-section",
             ), width=4
         ),
         dbc.Col(
@@ -104,10 +106,9 @@ layout = html.Div([
                                     className="btn btn-info") 
                                 ], width = 2),
                         ], justify= 'end', className = "details-table-nav"),
-                        
-                        dbc.Table(id = "predicted-table")
-                    ], className="card-body")
-                ], className= "card bg-light mb-3 projections-tables"
+                    ], className="card-body"),
+                    html.Div(dbc.Table(id = "predicted-table"), className = "table-box")
+                ], className= "card bg-light mb-3 projections-tables details-table-section"
             ), width=8
         ),
     ])
@@ -146,7 +147,6 @@ def filter_options(data):
     State("whse-selection", "value"),
     State("cat-selection", "value"),
     State("targets", "value"),
-    # Input("sales-store", "data"),
     Input("model-run", "n_clicks")
 )
 def date_picker(start, end, whse, cat, target, click):
@@ -156,8 +156,6 @@ def date_picker(start, end, whse, cat, target, click):
     end_date = end
     if click is None:
         raise PreventUpdate
-        # data = pd.DataFrame()
-        # data = data.to_json(date_format='iso') 
     else:
         # start_date = pd.Timestamp(start_date)
         # end_date = pd.Timestamp(end_date)
@@ -167,9 +165,9 @@ def date_picker(start, end, whse, cat, target, click):
         fh = ForecastingHorizon(
             pd.PeriodIndex(pd.date_range("2023-01", periods= 2, freq="M")), is_relative=False
         )
-        pred = forecaster.forecast(fh)
-        data = pred.to_json(date_format='iso')
-    return data
+        pred = forecastingPipeline.forecast(fh)
+        # data = pred.to_json(date_format='iso')
+    return pred
 
 
 @callback(
@@ -177,36 +175,41 @@ def date_picker(start, end, whse, cat, target, click):
     # Input("date-time-filter", "value"),
     Input("projected-data", "data")
 )
-def salesTrend(date, data):
-    plot_data = pd.DataFrame(json.loads(data))
-    plot_data = plot_data[plot_data["year"] == date]
-    plot_data = plot_data.groupby(["month_year", "month"]).sum("Order_Demand")
-    plot_data = pd.DataFrame(plot_data).reset_index()
-    fig = px.histogram(plot_data, y="Order_Demand", x="month", template="cyborg",  histfunc='sum', 
-                       labels = {'Order_Demand':'Orders', "month": "Month"})
-    
-    fig.update_layout(
-        paper_bgcolor = '#222',
-        margin={'l':20, 'r':20, 'b':0},
-        font_color='white',
-        # font_size=18,
-        style ={
-            'height': '300px'
-        },
-        hoverlabel={'bgcolor':'black', 'font_size':12, },
-        bargap=.40
+def sales_trend(data):
+    if data is None or data == "":
+        raise PreventUpdate
+    else: 
+        plot_data = pd.read_json(data)
+        # plot_data = plot_data[plot_data["year"] == date]
+
+        # plot_data["month"] = plot_data["month_year"].dt.strftime('%B')
+        plot_data = plot_data.groupby(["month_year"]).sum("Order_Demand")
+        plot_data = pd.DataFrame(plot_data).reset_index()
+        fig = px.histogram(plot_data, y="Order_Demand", x="month_year", template="cyborg",  histfunc='sum', 
+                        labels = {'Order_Demand':'Orders', "month_year": "Month"})
         
-    )
-    fig.update_traces(
-        # marker_bgcolor="#93c"
-        marker = {
-            'color': '#93c',
-        }
+        fig.update_layout(
+            paper_bgcolor = '#222',
+            margin={'l':20, 'r':20, 'b':0},
+            font_color='white',
+            # font_size=18,
+            # style ={
+            #     'height': '300px'
+            # },
+            hoverlabel={'bgcolor':'black', 'font_size':12, },
+            bargap=.40
+            
         )
-    fig.update_xaxes( # the y-axis is in dollars
-        dtick= 30, 
-        showgrid=True
-    )
+        fig.update_traces(
+            # marker_bgcolor="#93c"
+            marker = {
+                'color': '#93c',
+            }
+            )
+        fig.update_xaxes( # the y-axis is in dollars
+            dtick= 30, 
+            showgrid=True
+        )
     
     return fig
 @callback(
@@ -214,23 +217,41 @@ def salesTrend(date, data):
     Input("projected-data", "data")
 )
 def data_table(data):
-    data = pd.DataFrame(json.loads(data))
-    # data = data[(data["year"] == date )& (data["Warehouse"] == whse)]
-    # table = data[["Warehouse","Product_Category", "month_year","Order_Demand"]]
-    # table = table.groupby(["Warehouse","Product_Category", "month_year"], as_index = False).agg(
-    #     Demand = pd.NamedAgg(column = "Order_Demand", aggfunc = sum),
-    #     # Returns = pd.NamedAgg(column = "Returns", aggfunc = sum)
-    #)
-    df = dbc.Table.from_dataframe(data, striped=True, bordered=True, hover=True, index=True, responsive = True)
+    if data is None:
+        raise PreventUpdate
+    else: 
+        data = pd.DataFrame(json.loads(data))
+        # table = data[["Warehouse","Product_Category", "month_year","Order_Demand"]]
+        # table = table.groupby(["Warehouse","Product_Category", "month_year"], as_index = False).agg(
+        #     Demand = pd.NamedAgg(column = "Order_Demand", aggfunc = sum),
+        #     # Returns = pd.NamedAgg(column = "Returns", aggfunc = sum)
+        #)
+        df = dbc.Table.from_dataframe(data, striped=True, bordered=True, hover=True, index=True, responsive = True)
     return df
 
-# @callback(
-#     Output("whse-projection", "children"),
-#     State("targets", "value"),
-#     Input("projected-data", "data")
-# )
-# def whse_projections(data, value):
-#     data = pd.DataFrame(json.loads(data))
-#     df = data[["Warehouse", value]]
-#     df = data.groupby(["Warehouse"], as_index= false)["Order_Demand"].sum()
-#     return df
+@callback(
+    Output("cat-projection", "children"),
+    Input("projected-data", 'data')
+)
+def cat_rates(data):
+    data = pd.DataFrame(json.loads(data))
+    data = data[["Product_Category", "Order_Demand"]]
+    data = data.groupby(["Product_Category"], as_index = False).agg(
+        Demand = pd.NamedAgg(column = "Order_Demand", aggfunc = sum),
+        # Returns = pd.NamedAgg(column = "Returns", aggfunc = sum)
+    )
+    
+    cat_rated = data.sort_values('Demand', ascending=False).head(10)
+    cards = [dbc.Row([
+                dbc.Row([
+                    dbc.Col([
+                        html.I(className="bi bi-arrow-up-right-square-fill icon-main opacity")
+                    ], width = 1),
+                    dbc.Col([cat_rated.iloc[i,0],], className = 'border-right', width = 5),
+                    dbc.Col([numerize.numerize(cat_rated.iloc[i,1]),], className = 'border-right', width = 3),
+                    # dbc.Col([numerize.numerize(cat_rated.iloc[i,2]),], width = 3)  ,
+                ]),
+                html.Br(),
+                html.Hr() 
+            ]) for i in range(len(cat_rated))]
+    return cards
